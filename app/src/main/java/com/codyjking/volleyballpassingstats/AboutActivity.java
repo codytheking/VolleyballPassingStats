6d51a12e22b822e3558435471ce490e0b42e4eaa
codytheking <codytheking93@gmail.com>
2018-12-30 21:42:16 -0800

    Create About page for version number, privacy policy, etc.

---
+++ app/src/main/java/com/codyjking/volleyballpassingstats/AboutActivity.java
@@ -0,0 +1,24 @@
+package com.codyjking.volleyballpassingstats;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView v = findViewById(R.id.privacy_policy_text);
+        v.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
